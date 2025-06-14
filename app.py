3ddc49ace53dd80f54926976a1dd176635fa8652
Wuddke <invertedwuddle@gmail.com>
2025-06-15 07:13:19 +0800

    chore; 2 web pages

--- app.py
+++ app.py
@@ -12,7 +12,9 @@ def home():
 def services():
     return render_template("services.html")
 
-
+@app.route("/contact-us")
+def contact():
+    return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
